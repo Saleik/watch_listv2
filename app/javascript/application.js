183725a99f9edf0040210bb27eb9ab6187bbd186
kevin sarrazin <ksarrazin.dev@gmail.com>
2022-10-08 00:04:16 +0200

    [+][setup] rubocop config

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import 'bootstrap';
-import '@rails/request.js'
-import 'controllers'
-import "@hotwired/turbo-rails"
-import * as bootstrap from "bootstrap"
+import '@rails/request.js';
+import 'controllers';
+import '@hotwired/turbo-rails';
