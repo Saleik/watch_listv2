a58660a02b5fde1cb8dd4a1455b00b9521e171fe
kevin sarrazin <ksarrazin.dev@gmail.com>
2022-10-10 10:56:09 +0200

    [+][setup]wip

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,7 @@
 //= link_tree ../images
 //= link_tree ../builds
+//= link controllers/application.js
+
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
+//= link application.js
