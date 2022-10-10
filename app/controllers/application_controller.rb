4d3d44f298eba16e8ada9a5d5ac6f846d4c3f1a4
kevin sarrazin <ksarrazin.dev@gmail.com>
2022-10-10 20:55:25 +0200

    [+][create]create routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception, prepend: true
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nickname, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:nickname, :email, :password, :current_password) }
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+URL = 'https://tmdb.lewagon.com/movie/top_rated'.freeze
+
+fetched_movies = URI.open(URL).read
+movies = JSON.parse(fetched_movies)['results']
+
+puts 'Add 20 movies....'
+
+movies.each do |movie|
+  puts "Create #{movie['title']} movie"
+  new_movie = Movie.new(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: movie['poster_path'],
+    rating: movie['vote_average'],
+    year: movie['release_date'].split('-')[0]
+  )
+  puts 'Store it'
+  new_movie.save!
+end
+
+puts '20 movies successfully added to DB!!'
