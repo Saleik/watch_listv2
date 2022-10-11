ed174a03449a28c2219f57af860cc5b6bb9c24f3
kevin sarrazin <ksarrazin.dev@gmail.com>
2022-10-11 16:21:29 +0200

    [+][create][set-cloudinary] adding cloudinary setup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import 'bootstrap';
 import * as bootstrap from 'bootstrap';
+import "@fortawesome/fontawesome-free";
 import 'controllers';
 import '@hotwired/turbo-rails';
--- config/importmap.rb
+++ config/importmap.rb
@@ -9,6 +9,7 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js'
 pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.0/lib/index.js'
+pin "@fortawesome/fontawesome-free", to: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
