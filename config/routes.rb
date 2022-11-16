263fef8590f206664e207a2b997eb1eaf4ba4512
kevin sarrazin <ksarrazin.dev@gmail.com>
2022-11-16 16:29:29 +0100

    [+][create]update danger bootstrap color + create lits controller + index view + list/card partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'pages#home'
+  root to: 'lists#index'
 
-  resources :lists, only: [:index, :new, :create, :show, :edit] do
+  resources :lists, only: [:new, :create, :show, :edit] do
     resources :bookmarks, only: [:new, :create]
   end
 
