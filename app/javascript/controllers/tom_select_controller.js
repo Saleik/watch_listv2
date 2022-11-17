056bb8842bc17f1522b76d6d12d0aaeb839c83ef
kevin sarrazin <ksarrazin.dev@gmail.com>
2022-11-17 17:24:29 +0100

    [~][create]wip form list added tom-select package

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -1,8 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
+import TomSelect from "tom-select";
 
-// Connects to data-controller="hello"
+// Connects to data-controller="tom-select"
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    console.log('toto')
   }
 }
