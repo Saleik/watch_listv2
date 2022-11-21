29b4eb2ee0f0074c834cdbfe8569bd9f8b0afa4a
kevin sarrazin <ksarrazin.dev@gmail.com>
2022-11-21 10:06:21 +0100

    [~][create]wip tom select list form

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -4,6 +4,6 @@ import TomSelect from "tom-select";
 // Connects to data-controller="tom-select"
 export default class extends Controller {
   connect() {
-    console.log('toto')
+    new TomSelect(this.element)
   }
 }
