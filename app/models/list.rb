becc5213eddc23221febae370990554d7b1af814
kevin sarrazin <ksarrazin.dev@gmail.com>
2022-11-30 18:06:20 +0100

    [+][add] wip form save model side + adding turbo stream behavior

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,8 +3,22 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks
   has_many :bookmarks, dependent: :destroy
   has_one_attached :image
-  accepts_nested_attributes_for :bookmarks, reject_if: ->(attributes) { attributes["comment"].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :bookmarks, allow_destroy: true
+
+  after_commit :store_bookmarks, on: %i[create]
 
   validates :name, presence: true, uniqueness: { scope: :user_id }
   validates :resume, presence: true, length: { maximum: 150 }
+
+  def store_bookmarks
+    # movie.each do |m|
+    #   if m.size.positive?
+    #     bookmark = Bookmark.new(
+    #       list: self,
+    #       movie: m
+    #     )
+    #     bookmark.save!
+    #   end
+    # end
+  end
 end
